917d9f4f4c0618470462ede3868bc6b5e43d4163
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-06 02:24:23 +0530

    Added unread message concept

--- src/ViewMessagePage.js
+++ src/ViewMessagePage.js
@@ -11,6 +11,7 @@ const DISPLAY_MESSAGE = 'displayMessage'
 const READ_STATUS = 'readStatus';
 // const SELECTED_MESSAGE = 'selectedMessage'
 const MESSAGE_NUMBER = 'message_number';
+const CHECKBOX = 'checkbox';
 
 
 const ViewMessagePage = () => {
@@ -30,25 +31,33 @@ const ViewMessagePage = () => {
 
     const [highlightMessage, setHighlightMessage] = useState(() => readFromStorage(MESSAGE_NUMBER) || 0);
 
+    const [checkStatus, setCheckBox] = useState(() => readFromStorage(CHECKBOX) || false);
+    // const [checkStatus, setCheckBox] = useState(() => readFromStorage(CHECKBOX) || false);
+
 
 
     const goToMessage = (messageNumber) => {
-        console.log(dispMsg[messageNumber]);
-        // setStep(step);
         setHighlightMessage(messageNumber);
-        // console.log(highlightMessage);
         return dispMsg[messageNumber];
     };
 
-    // const jumpToState = (step) => {
-    //     setStep(step);
-    // };
 
     useEffect(() => {
         writeToStorage(READ_STATUS, readMessage);
+        writeToStorage(CHECKBOX, checkStatus);
     });
 
+    useEffect(() => {
+        writeToStorage(CHECKBOX, checkStatus);
+    },[checkStatus]);
 
+    useEffect(() => {
+        setCheckBox(false);
+    },[highlightMessage]);
+
+
+
+    
     function renderOldMessages() {
         if (dispMsg.length === 0 || dispMsg === null) {
             return (
@@ -57,7 +66,7 @@ const ViewMessagePage = () => {
         } else {
             return dispMsg.map((b, index) => (
                 <li key={index}>
-                    <h6 class={cn('NewMessage', { ReadMessage: readMessage[index]==='read' })}>New</h6>
+                    <h6 class={cn('NewMessage', { ReadMessage: readMessage[index] === 'read' })}>New</h6>
                     <div class="MessageContent">
                         <div class="MessageHistoryNumber"
                             onClick={() => {
@@ -79,9 +88,9 @@ const ViewMessagePage = () => {
 
                                 readMessage[index] = 'read';
 
-                                console.log(readMessage[index]);
+                                // console.log(readMessage[index]);
 
-                                console.log(readMessage);
+                                // console.log(readMessage);
 
                                 setReadMessage(readMessage);
                                 // writeToStorage(READ_STATUS, readMessage); //PARTIALLY WORKING......
@@ -120,7 +129,22 @@ const ViewMessagePage = () => {
                 </form>
 
 
-                <input class="CheckBox" type="checkbox" id="vehicle1" name="vehicle1" value="Bike">
+                <input class="CheckBox"
+                    type="checkbox"
+                    id="vehicle1"
+                    name="vehicle1"
+                    value="Bike"
+                    checked={checkStatus}
+                    // checked={readMessage[highlightMessage]==='read'}
+                    onClick={() => {
+                        console.log("YUVRAJ SINGH......");
+                        console.log(highlightMessage);
+                        setCheckBox(true);
+
+                        readMessage[highlightMessage] = 'unread';
+                        setReadMessage(readMessage);
+                    }}
+                >
 
                 </input>
                 <label for="messageRead"> mark message as unread</label><br></br>
